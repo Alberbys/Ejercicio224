fe15a80dd9a9547337f2c36c9ba56cfe9eafb693
alber <alberbys2003@gmail.com>
2023-11-19 17:55:30 -0600

    Initial commit

---
+++ app/src/main/java/com/example/ejercicio224/MyDatabaseHelper.java
@@ -0,0 +1,30 @@
+package com.example.ejercicio224;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "mi_basededatos.db";
+    private static final int DATABASE_VERSION = 1;
+    public static  final String tableName = "firmas";
+
+    public MyDatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public static final String SelectTable= "SELECT * FROM " + tableName;
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTableQuery = "CREATE TABLE firmas(id INTEGER PRIMARY KEY AUTOINCREMENT, signature BLOB, nombre TEXT)";
+
+
+        db.execSQL(createTableQuery);
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+}
